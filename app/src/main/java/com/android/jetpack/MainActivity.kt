887373960e5488c2ba9f4ede3196397d8d953f39
qinwei <qinwei_it@163.com>
2020-11-27 19:55:50 +0800

    init project

---
+++ app/src/main/java/com/android/jetpack/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.android.jetpack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.qw.framework.App
+import com.qw.framework.AppStateTracker
+import com.qw.framework.ui.QFragmentActivity
+import com.qw.framework.ui.SupportFragmentListActivity
+import com.qw.jetpack.workmanger.WorkManagerFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        AppStateTracker.init(application)
+        AppStateTracker.getInstance().appState = AppStateTracker.APP_STATE_ONLINE
+        App.init(application, MainActivity::class.java)
+        val cs = ArrayList<QFragmentActivity.Clazz>()
+        cs.add(QFragmentActivity.Clazz("WorkManager", WorkManagerFragment::class.java))
+        SupportFragmentListActivity.startActivity(this, cs)
+        finish()
+    }
+}
---
+++ jetpack/src/main/java/com/qw/jetpack/Utils.java
@@ -0,0 +1,8 @@
+package com.qw.jetpack;
+
+/**
+ * Created by qinwei on 2020/11/27 6:41 PM
+ * email: qinwei_it@163.com
+ */
+class Utils {
+}
