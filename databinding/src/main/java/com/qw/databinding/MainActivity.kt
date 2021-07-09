fd6e487ced5135b8919778b9ade9d46156e126e5
qinwei <qinwei_it@163.com>
2021-07-09 14:02:37 +0800

    数据绑定

--- databinding/src/main/java/com/qw/databinding/MainActivity.kt
+++ databinding/src/main/java/com/qw/databinding/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.qw.databinding
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.qw.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val user=User("张三丰",80)
+        binding.user=user
+        user.age++
     }
 }
---
+++ databinding/src/main/java/com/qw/databinding/User.kt
@@ -0,0 +1,3 @@
+package com.qw.databinding
+
+data class User( var name:String,var age:Int)
